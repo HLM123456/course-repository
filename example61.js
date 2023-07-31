2fd86f5ed8d7af4a7e49c54110250660dab295e4
HLM123456 <126587525+HLM123456@users.noreply.github.com>
2023-07-31 16:56:01 +0800

    Add files via upload

---
+++ example61.js
@@ -0,0 +1,20 @@
+
+function question1(){
+  var response=prompt("您的用户名是?");
+  alert("您好"+response+"，现在可以开始答题啦！");
+}
+ function bingo1(){
+   prompt("答对啦！");
+ }
+ function bingo2(){
+   var response=prompt("答对啦！");
+ }
+ function bingo3(){
+   var response=prompt("答对啦！");
+ }
+ function bingo4(){
+   var response=prompt("答对啦！");
+ }
+ function bingo5(){
+   var response=prompt("答对啦！");
+ }
